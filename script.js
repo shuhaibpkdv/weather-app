37499b643bb1834728d2db8567ac0848e43afb32
muhammed-shuhaib <muhammedshuhaibpkdv@gmail.com>
2025-08-17 20:58:04 +0530

    called api, givened right values

--- script.js
+++ script.js
@@ -1,18 +1,29 @@
-function Sample() {
-  let input = document.getElementById('place-input');
+let input = document.getElementById('place-input');
+const resultCard = document.getElementById('weather-condition-container');
+const apiKey = '70b0f4523fd19a2090bf86d3cf6de2ff';
 
-  html = `
-    <h2 class="weather-place-name">${input.value}</h2>
-    <h2 class="weather-degree">27°C</h2>
-  `
+function Sample() {
+  const city = input.value;
 
   if(input.value == "") {
     alert("Please Enter a Place Name");
-  } else {
-    document.getElementById('weather-condition-container')
-      .innerHTML = html;
   }
+
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+
+      resultCard
+        .innerHTML = `
+          <h2 class="weather-place-name">${data.name}, ${data.sys.country}</h2>
+          <h2 class="weather-degree">${data.main.temp}°C</h2>
+        `
+    });
 
   input.value = "";
 }
+
 
